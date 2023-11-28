0f867be11a9d38477f7339050698250809396519
CarlosChiva <cxhiva@gmail.com>
2023-11-28 20:48:16 +0100

    run work, taste call to another task

--- tasks.py
+++ tasks.py
@@ -4,3 +4,15 @@ from invoke import task
 @task(name="hello")
 def hello(c):
     print("hello-world")
+@task(name="run")
+def run(c):
+    hello(c)    
+@task(name="retu_string")
+def retu_string(s)->str:
+    return str(c)+"hello motherfucker"
+@task(name="return_int")
+def return_int(c)->int:
+    return 1    
+@task(name="main")
+def main(m):
+    print(retu_string(return_int()))
